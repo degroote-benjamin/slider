f36d3fdba36294b75ac4ca494598fd065a2e8764
benjamin <degroote.benjamin@gmail.com>
2017-07-12 14:54:32 +0200

    add first script for add first button click on result

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 // init variable
   var i =0
   var img =document.getElementsByClassName("img")
-
+  var calcul = document.getElementsByClassName("calculet")
+  var tabcalcul= []
+  var tabtotal = []
+  var total = 0
+  var resultat = document.getElementById("resultatcalcul")
+  resultat.innerHTML = 0
 // On load page , slider is on
   document.addEventListener("load", function(){
     slider()
@@ -10,11 +15,11 @@
 
 // function slider
 function slider() {
-    for(let y=0;y < img.length ; y++){
-     img[y].style.display = "none";
+    for(let y=0;y <img.length ; y++){
+     img[y].style.display = "none"
    }
 
-    img[i].style.display = "block";
+    img[i].style.display = "block"
     i++
     if(i === img.length){
       i=0
@@ -23,3 +28,23 @@ function slider() {
 
 // function slider all 3sec
 setInterval(slider, 3000);
+
+for (let i = 0; i < calcul.length; i++) {
+    calcul[i].addEventListener("click", function() {
+tabcalcul.push(calcul[i].innerHTML)
+
+if(tabcalcul.length ===1)
+{
+  total = tabcalcul[0]
+}
+
+if(tabcalcul.length ===3){
+  if(tabcalcul[1] === "+"){
+    total = Number(tabcalcul[0]) + Number(tabcalcul[2]) + total
+  }
+  // else if(tabcalcul[1] = ".")
+}
+resultat.innerHTML =total
+}
+)
+}
