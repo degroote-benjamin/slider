a9192ce416f74a412a09be1f689ab0c1df0faa47
benjamin <degroote.benjamin@gmail.com>
2017-07-13 13:43:56 +0200

    add right bouton on slider with script

--- js/main.js
+++ js/main.js
@@ -12,22 +12,33 @@ document.addEventListener("load", function() {
     slider()
 });
 
+document.getElementById("gauche").addEventListener("click", function() {
+   --i
+   setTimeout(slider,0)
+});
+
+document.getElementById("droite").addEventListener("click", function() {
+    setTimeout(slider,0)
+});
+
 // function slider
 function slider() {
     for (let y = 0; y < img.length; y++) {
         img[y].style.display = "none"
     }
-
-    img[i].style.display = "block"
-    i++
-    if (i === img.length) {
+    if (i >=img.length || i <0 ) {
         i = 0
     }
+    img[i].style.display = "block"
+    i++
 }
 
 // function slider all 3sec
 setInterval(slider, 3000);
 
+
+
+// calculator
 for (let i = 0; i < calcul.length; i++) {
     calcul[i].addEventListener("click", function() {
 
@@ -88,7 +99,6 @@ for (let i = 0; i < calcul.length; i++) {
             } else {
               resultat.innerHTML = "error"
               tabcalcul.length = 0
-              total = 0
             }
         }
 
